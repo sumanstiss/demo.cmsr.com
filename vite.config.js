b1c019fb7f01b749adea6367ee12876e0e491446
sumanstiss <sumanstiss@gmail.com>
2025-11-23 22:26:08 +0530

    Set base URL for Vite configuration
    Updated the base URL in the Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/https://github.com/sumanstiss/demo.cmsr.com/', // <--- replace REPO_NAME with your repo name
+  plugins: [react()]
 })
 
