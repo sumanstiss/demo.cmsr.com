d50a0495208516683372fa8999fe3c83a4b7a6e8
sumanstiss <sumanstiss@gmail.com>
2025-11-23 22:28:47 +0530

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/https://github.com/sumanstiss/demo.cmsr.com/', // <--- replace REPO_NAME with your repo name
+  base: '/demo.cmsr.com/', // <--- replace REPO_NAME with your repo name
   plugins: [react()]
 })
 
